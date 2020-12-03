a73249c26f71c462230ca1689fe5d568e680872a
Sylvain COULOMBEL <sylvaincoulombel@gmail.com>
2020-12-03 22:43:52 +0100

    Add a cli to test the lib (with http query parameters), docker and add CI

---
+++ README.md
@@ -0,0 +1,32 @@
+# README
+
+## Run from Python 
+
+````shell script
+python cli.py -X GET --path "https://attestationcovid.site:443/" --header "Accept: */*" --header "yoloheader: scoulomb"
+````
+
+
+## Run from Docker
+
+
+### RAW Docker
+
+````shell script
+docker build . -f Dockerfile -t socket-curl
+docker run socket-curl -X GET --path "https://attestationcovid.site:443/" --header "Accept: */*" --header "yoloheader: scoulomb"
+````
+
+### Compose
+
+Edit compose file and run 
+
+````shell script
+docker-compose up --build
+````
+
+## Known limitations
+
+- We do not handle well the POST with a body (sadly)
+- This client managed content-length only but some server returns a Transfer-Encoding with the size of the chunk
+See: https://tools.ietf.org/html/rfc7230#section-3.3.2
---
+++ cli.py
@@ -0,0 +1,68 @@
+import argparse
+import urllib
+from argparse import Namespace
+from urllib.parse import urlparse
+
+from main import Connection, Request, send
+
+
+def _read_cli_arguments() -> Namespace:
+    parser = argparse.ArgumentParser(
+        description="CLI for HTTP over Socket")
+
+    parser.add_argument(
+        "-X",
+        type=str,
+        default="GET",
+        choices=["GET", "POST", "PUT", "DELETE", "PATCH"],
+        help="HTTP method")
+
+    parser.add_argument(
+        "--path",
+        type=str,
+        help="url")
+
+    parser.add_argument(
+        "--header",
+        type=str,
+        help="url",
+        action="append",
+        nargs="+"
+    )
+
+    parser.add_argument(
+        "--body",
+        default=None,
+        required=False,
+        type=str,
+        help="body")
+
+    return parser.parse_args()
+
+
+def main():
+    inputs: argparse.Namespace = _read_cli_arguments()
+    http_method = inputs.X
+    headers_list = []
+    for header in inputs.header:
+        headers_list.append(header[0])
+
+    # https://stackoverflow.com/questions/9626535/get-protocol-host-name-from-url
+    # https: //docs.python.org/3/library/urllib.parse.html#urllib.parse.urlsplit
+    url_split = urllib.parse.urlsplit(inputs.path)
+
+    protocol = url_split.scheme
+    netloc = url_split.netloc
+    path = f"{url_split.path}?{url_split.query}" if url_split.query != "" else url_split.path
+    hostname, _, port = netloc.partition(":")
+    port_as_int = int(port)
+
+    connection = Connection(hostname, port_as_int, protocol=protocol)
+    request = Request(http_method, path, headers_list, body=inputs.body, timeout_seconds=25)
+    print(request)
+    response = send(connection, request)
+    print(response)
+
+
+if __name__ == "__main__":
+    main()
